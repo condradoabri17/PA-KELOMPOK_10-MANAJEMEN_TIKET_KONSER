8465e65a28ef14ffd755c086309a23b177957718
condradoabri17 <condradoabri17@gmail.com>
2025-10-14 07:24:02 +0800

    fitur login

--- MANAJEMEN-TIKET-KONSER.py
+++ MANAJEMEN-TIKET-KONSER.py
@@ -23,4 +23,37 @@ def login_user():
         
         if ditemukan == True:
             print("Login Berhasil")
-            print("-" * 90)
+
+            return True
+        else :
+            print("Username atau Password salah")
+            return False
+    else:
+        print("Buatkan akun baru")
+        username = input("Username Baru: ")
+        password = input("password Baru: ")
+        role = "user"
+        saldo = 0
+
+        akun_baru = {"username" : username, "password" : password, "role" : role, "saldo" : saldo}
+        data.append(akun_baru)
+        with open("akun.json", "w") as f:
+            json.dump(data, f, indent=4)
+        print("akun berhasil dibuat")
+        return akun_baru
+
+
+# print("\nmenu utama")
+# while True:
+
+#     print("\nSelamat datang di sistem manajemen tiket konser\nSilahkan Login!!")
+
+#     konfirmasi = 'Y'
+#     while konfirmasi == "Y":
+#         akun = login_user()
+#         if akun["role"] == "admin":
+#             print("masuk men")
+#         elif:
+#             print("akun pembeli")
+        
+#         input("Ingin Melanjutkan? =")
